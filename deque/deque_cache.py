bc2bc52cbb2ad374d8c5fd623ae5401c6b7ca045
thajatakuma <thajahidemitakuma@gmail.com>
2023-06-24 21:13:36 -0300

    adjust black

--- deque/deque_cache.py
+++ deque/deque_cache.py
@@ -1,26 +1,29 @@
-from collections import deque 
+from collections import deque
 
 # posso definir o tamanho máximo da minha deque
 cache_values = deque(maxlen=5)
 
+
 def cache(func):
     def inner(*args):
         cache_values.append(args)
-        return  func(*args)
+        return func(*args)
+
     return inner
+
 
 @cache
 def soma(x, y):
     return x + y
 
 
-somar_1 = soma(2,2)
-somar_2 = soma(3,4)
-somar_3 = soma(5,1)
-somar_4 = soma(7,7)
+somar_1 = soma(2, 2)
+somar_2 = soma(3, 4)
+somar_3 = soma(5, 1)
+somar_4 = soma(7, 7)
 
 # insere no incio
-cache_values.appendleft((1,1))
+cache_values.appendleft((1, 1))
 print(cache_values)
 
 # reverte a ordem
@@ -33,4 +36,3 @@ print(cache_values)
 
 cache_values.rotate(-3)
 print(cache_values)
-
